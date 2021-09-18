e5bacd1e39b35ebaf39949493c3599256dd1ee15
samuel <it.fischer.samuel@gmail.com>
2021-09-19 00:14:57 +0200

    make it better

---
+++ index.js
@@ -0,0 +1,26 @@
+const axios = require("axios")
+const express = require("express")
+
+const app = express()
+
+app.get("/", async (req, res) => {
+  const resul = await hack()
+  res.send(`
+  <html>
+    <head>
+      <title>Test orari</title>
+    </head>
+    <body>
+      <iframe src="${resul}" width="100%" height="100%"></iframe>
+    </body>
+  </html>
+  `)
+})
+app.listen(8080, () => { console.log("start") })
+
+async function hack() {
+  const url = "https://jozefstefan.org/wp-json/wp/v2/media/"
+  const response = await axios.get(url)
+  const data = response.data
+  return (data.map(d => d.link).find(s => s.includes("urnik")));
+}
