0f4750411065d09f80b3e38b7b07a8b587d8224d
samuel <it.fischer.samuel@gmail.com>
2021-09-19 01:06:02 +0200

    make it better

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ app.get("/", async (req, res) => {
   const resul = await hack()
   res.send(`
   <html>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
     <head>
       <title>Test orari</title>
     </head>
     <body>
-      <iframe src="${resul}" width="100%" height="100%"></iframe>
+      <iframe id="pdfviewer" src="http://docs.google.com/gview?embedded=true&url=${resul}&amp;embedded=true" frameborder="0" width="100%" height="100%"></iframe>
     </body>
   </html>
   `)
