83f0b219a07012165e0ea773f34e4c3b8a8627e9
samuel <it.fischer.samuel@gmail.com>
2021-09-23 20:04:56 +0200

    make it better

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var path = require('path')
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.listen(process.env.PORT, () => { console.log("start") })
+//app.listen(process.env.PORT, () => { console.log("start") })
+app.listen(8080, () => { console.log("start") })
 
 app.get("/", async (req, res) => {
   let map = await hack()
@@ -19,8 +20,8 @@ app.get("/", async (req, res) => {
       <link rel='icon' href='/images/favicon.ico' class='js-favicon'>
     </head>
     <body style="background-color:black;">
-      <iframe id="pdfviewer" src="http://docs.google.com/gview?embedded=true&url=${resul0}&amp;embedded=true"" frameborder="0" width="100%" height="100%"></iframe>
-      <iframe id="pdfviewer" src="http://docs.google.com/gview?embedded=true&url=${resul1}&amp;embedded=true"" frameborder="0" width="100%" height="100%"></iframe>
+      <iframe id="pdfviewer" src="http://docs.google.com/gview?embedded=true&url=${resul0}" frameborder="0" width="100%" height="100%"></iframe>
+      <iframe id="pdfviewer" src="http://docs.google.com/gview?embedded=true&url=${resul1}" frameborder="0" width="100%" height="100%"></iframe>
     </body>
   </html>
   `)
@@ -30,5 +31,5 @@ async function hack() {
   const url = "https://jozefstefan.org/wp-json/wp/v2/media/"
   const response = await axios.get(url)
   const data = response.data
-  return (data.map(d => d.link).filter(s => s.includes("urnik")));
+  return (data.map(d => d.link).filter(s => s.includes("urnik-za")));
 }
